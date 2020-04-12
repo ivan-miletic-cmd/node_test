e6f0a920eaa424d70be6d08436168382252afeb2
ivan_miletic_cmd <ivan@qpreems.com>
2020-04-12 22:13:46 +0200

    Working with input example email

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+function checkMail(someMail){
+    if (someMail.indexOf('@') === true) {
+        return someMail;
+    };
+    return 'put the right email address';
+};
 
-const App = () => {
+
+const App = () => { 
     return (
+        <div>
         <h3>Hi world</h3>
+        <input type='text' id='mail'></input>
+        <label type='text'></label>
+        <button onClick = {() => alert(checkMail('somemail'))}>Click HERE!</button>
+        </div>
     );
 };
 
 ReactDOM.render(
     <App />,
     document.querySelector('#root')
-)
+);
