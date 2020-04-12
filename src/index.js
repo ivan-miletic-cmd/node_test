b5d8ba76f1eb1383da0e22a3e784f2a2749967bd
ivan_miletic_cmd <ivan@qpreems.com>
2020-04-12 13:57:19 +0200

    First output

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+const App = () => {
+    return (
+        <h3>Hi world</h3>
+    );
+};
+
+ReactDOM.render(
+    <App />,
+    document.querySelector('#root')
+)
